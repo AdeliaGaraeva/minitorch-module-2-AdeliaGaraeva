8962dbac612a1bcb7d88138b1b70ba11a7fe2d8c
AdeliaGaraeva <147227784+AdeliaGaraeva@users.noreply.github.com>
2025-09-23 13:51:02 +0300

    Update autodiff.py

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
-from typing import Any, Iterable, List, Tuple
-
+from typing import Any, Iterable, Tuple
+import queue
 from typing_extensions import Protocol
 
 # ## Task 1.1
@@ -22,7 +22,13 @@ def central_difference(f: Any, *vals: Any, arg: int = 0, epsilon: float = 1e-6) 
     Returns:
         An approximation of $f'_i(x_0, \ldots, x_{n-1})$
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    vals_eps = []
+    for i in range(len(vals)):
+        vals_eps.append(vals[i])
+        if i == arg:
+            vals_eps[i] += epsilon
+
+    return (f(*vals_eps) - f(*vals)) / epsilon
 
 
 variable_count = 1
@@ -60,7 +66,23 @@ def topological_sort(variable: Variable) -> Iterable[Variable]:
     Returns:
         Non-constant Variables in topological order starting from the right.
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    res = []
+    visited = set()
+
+    def dfs(node: Variable) -> None:
+        if node.is_constant():
+            return
+        if node.unique_id in visited:
+            return
+        visited.add(node.unique_id)
+        for par in node.parents:
+            dfs(par)
+
+        res.append(node)
+
+    dfs(variable)
+    res = res[::-1]
+    return res
 
 
 def backpropagate(variable: Variable, deriv: Any) -> None:
@@ -74,7 +96,21 @@ def backpropagate(variable: Variable, deriv: Any) -> None:
 
     No return. Should write to its results to the derivative values of each leaf through `accumulate_derivative`.
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    accum_derivatives = dict()
+    accum_derivatives[variable.unique_id] = deriv
+    for var in topological_sort(variable):
+        if var.unique_id in accum_derivatives.keys():
+            d_output = accum_derivatives[var.unique_id]
+        else:
+            d_output = 0.0
+        if var.is_leaf():
+            var.accumulate_derivative(d_output)
+        else:
+            for parent, d_parent in var.chain_rule(d_output):
+                if parent.unique_id in accum_derivatives.keys():
+                    accum_derivatives[parent.unique_id] += d_parent
+                else:
+                    accum_derivatives[parent.unique_id] = d_parent
 
 
 @dataclass
